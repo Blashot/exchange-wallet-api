73b29a9a386b723e566874e79f8eb067b547f30f
Blashot <malkowski.arthur@gmail.com>
2026-05-31 12:44:39 +0200

    initial template

---
+++ src/Application/Users/Register/UserRegisteredDomainEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Users;
+using SharedKernel;
+
+namespace Application.Users.Register;
+
+internal sealed class UserRegisteredDomainEventHandler : IDomainEventHandler<UserRegisteredDomainEvent>
+{
+    public Task Handle(UserRegisteredDomainEvent domainEvent, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
+
+internal sealed class UserRegisteredDomainEventHandler1 : IDomainEventHandler<UserRegisteredDomainEvent>
+{
+    public Task Handle(UserRegisteredDomainEvent domainEvent, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
